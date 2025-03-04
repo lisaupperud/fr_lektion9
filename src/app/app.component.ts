b528fd5cc3aa531a3b20d52fde84366c446c1490
liluppis <lisa.upp@hotmail.com>
2025-03-04 10:22:40 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -5,8 +6,28 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'lektion_9';
+
+  // prepare API
+  // detta kommer bli vår JSON
+  productData: any;
+
+  // private - för att komma åt utanför scopet
+  constructor(private httpClient: HttpClient) {}
+
+  // on creation of component -- så fort komponenten skapas
+  // ngOnInit = always executes once
+  ngOnInit() {
+    console.log('Hello World');
+
+    // subscribe: invänta data -> lite som ett promise -> inväntandet sker per automatik
+    this.httpClient
+      .get('https://fakestoreapi.com/products')
+      .subscribe((response) => {
+        this.productData = response;
+      });
+  }
 }
